18d1a21d9c778129e733736774feccc837394330
magdy288 <magdy288m@gmail.com>
2025-03-21 22:23:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,9 +66,9 @@ def vector_embedding(uploaded_file=None):
     if uploaded_file:
         with open(os.path.join('./pdfs', uploaded_file.name), 'wb') as f:
             f.write(uploaded_file.getbuffer())
-        st.session_state.loader = PyPDFDirectoryLoader('./pdfs')
+        st.session_state.loader = PyPDFDirectoryLoader('pdfs')
     else:
-        st.session_state.loader = PyPDFDirectoryLoader('./pdfs')
+        st.session_state.loader = PyPDFDirectoryLoader('pdfs')
         
     
     st.session_state.docs = st.session_state.loader.load()
